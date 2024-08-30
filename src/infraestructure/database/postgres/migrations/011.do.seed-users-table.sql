d2d48e2d214c3c4605e50790b3b338dee75ec353
Jose Miguel Dager Montoya <jdager@sdmujer.gov.co>
2024-08-29 19:56:36 -0500

    feat: added seeders to migrations

--- src/infraestructure/database/postgres/migrations/011.do.seed-users-table.sql
+++ src/infraestructure/database/postgres/migrations/011.do.seed-users-table.sql
@@ -1,2 +1,2 @@
-INSERT INTO persons (id, firstname, lastname, email, id_number) VALUES ('admin', 'admin', 'admin@admin.com', '123456789');
-INSERT INTO users (id, username, password, id_roles, id_persons) VALUES ('admin', '$2b$10$puirOS7erp37mrg88/00w.aaVHVSnkfKnkQrbdXumkS9eU6OVJ7s.', 1);
+INSERT INTO persons (id, firstname, lastname, email, id_number) VALUES (1, 'admin', 'admin', 'admin@admin.com', '123456789');
+INSERT INTO users (id, username, password, id_roles, id_persons) VALUES (1, 'admin', '$2b$10$puirOS7erp37mrg88/00w.aaVHVSnkfKnkQrbdXumkS9eU6OVJ7s.', 1);
